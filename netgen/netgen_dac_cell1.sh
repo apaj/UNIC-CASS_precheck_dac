08796f4736eef1de9884774ec643db23fe305ae5
apaj <aleksandar.pajkanovic@gmail.com>
2023-10-26 01:01:23 +0200

    precheck passing

--- netgen/netgen_dac_cell1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-netgen -batch lvs "../xschem/user_analog_project_wrapper.spice user_analog_project_wrapper" "../mag/user_analog_project_wrapper.spice user_analog_project_wrapper" /opt/open_pdks/sky130/sky130A/libs.tech/netgen/setup.tcl
